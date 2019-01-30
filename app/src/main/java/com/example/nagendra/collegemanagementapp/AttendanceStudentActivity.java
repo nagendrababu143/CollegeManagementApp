79c78b407c4087b198f1ac2c64b98d1619fb13c8
nagendrababu143 <31370179+nagendrababu143@users.noreply.github.com>
2019-01-30 12:24:14 +0530

    Initial commit

--- app/src/main/java/com/example/nagendra/collegemanagementapp/AttendanceStudentActivity.java
+++ app/src/main/java/com/example/nagendra/collegemanagementapp/AttendanceStudentActivity.java
@@ -2,12 +2,41 @@ package com.example.nagendra.collegemanagementapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class AttendanceStudentActivity extends AppCompatActivity {
+
+    EditText studentname_myattendance,month_myattendance;
+    Button attendeddays_myattendance_btn;
+    TextView attendeddays_myattendance;
+
+    SqliteHelper sqliteHelper = new SqliteHelper(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attendance_student);
+
+        studentname_myattendance = (EditText)findViewById(R.id.studentname_myattendance);
+        month_myattendance = (EditText)findViewById(R.id.month_myattendance);
+        attendeddays_myattendance_btn = (Button)findViewById(R.id.attendeddays_myattendance_btn);
+        attendeddays_myattendance = (TextView)findViewById(R.id.attendeddays_myattendance);
+
+        attendeddays_myattendance_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String studentname_myattendance_text = studentname_myattendance.getText().toString().trim();
+                String month_myattendance_text = month_myattendance.getText().toString().trim();
+
+                int count = sqliteHelper.getTheCount(studentname_myattendance_text,month_myattendance_text);
+
+                attendeddays_myattendance.setText(String.valueOf(count));
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/nagendra/collegemanagementapp/TestmarksStudentActivity.java
+++ app/src/main/java/com/example/nagendra/collegemanagementapp/TestmarksStudentActivity.java
@@ -2,12 +2,43 @@ package com.example.nagendra.collegemanagementapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class TestmarksStudentActivity extends AppCompatActivity {
+
+    EditText studentname_myassessment,month_myassessment;
+    Button marks_btn_myassessment;
+    TextView totalmarks_myassessment;
+
+    SqliteHelper sqliteHelper = new SqliteHelper(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_testmarks_student);
+
+        studentname_myassessment = (EditText)findViewById(R.id.studentname_myassessment);
+        month_myassessment = (EditText)findViewById(R.id.month_myassessment);
+        marks_btn_myassessment = (Button)findViewById(R.id.marks_btn_myassessment);
+        totalmarks_myassessment = (TextView)findViewById(R.id.totalmarks_myassessment);
+
+        marks_btn_myassessment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String studentname_myassessment_text = studentname_myassessment.getText().toString().trim();
+                String month_myassessment_text = month_myassessment.getText().toString().trim();
+
+                int val = sqliteHelper.totalMarks(studentname_myassessment_text,month_myassessment_text);
+
+                //String val1 = val;
+                totalmarks_myassessment.setText(String.valueOf(val));
+            }
+        });
+
+
     }
 }
